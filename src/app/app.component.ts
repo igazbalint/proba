7c116c7a0e0e455742fbb1846f7b8e2cb95ead6c
Tsukiyadesu <124679441+Tsukiyadesu@users.noreply.github.com>
2023-03-01 10:26:03 +0100

    test btn disabled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'feladat';
-  numbers:any = [];
-  generateNumbers(quantity: number = 10){
-    for(let i = 0; i < quantity; i++){
-    
-      this.numbers.push( Math.floor(Math.random() * (5000 - 3 + 1) + 3))
-     console.log(this.numbers);
-    }
+  numbers: any = [];
 
+  btnpressed: boolean = false;
+
+  generateNumbers(quantity: number = 10) {
+    for (let i = 0; i < quantity; i++) {
+      this.numbers.push(Math.floor(Math.random() * (5000 - 3 + 1) + 3));
+      console.log(this.numbers);
+      this.btnpressed = true;
+      return this.numbers;
+    }
   }
-   
 
 }
