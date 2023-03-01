ab4e309590da3b925d40495f843221017e528df0
Tsukiyadesu <124679441+Tsukiyadesu@users.noreply.github.com>
2023-03-01 12:08:20 +0100

    not done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,37 @@ export class AppComponent {
       this.numbers.push(Math.floor(Math.random() * (5000 - 3 + 1) + 3));
       console.log(this.numbers);
       this.btnpressed = true;
-      return this.numbers;
     }
+    return this.numbers;
   }
+  
 
+  countPrimes(numbersArray: any[]) {
+    const startTime = new Date();
+    var result = {}
+    for (let i = 0; i < numbersArray.length; i++) {
+      const primes = [];
+      let count = 0;
+      let found = false;
+
+      for (let j = 2; j < numbersArray[i] / 2; i++) {
+        if (numbersArray[i] % 2 == 0) {
+          primes.push(numbersArray[i]);
+          found = true;
+          count++;
+        }
+      }
+      result = {
+        primes:primes,
+        count:count,
+        fount:found
+        }
+      console.log(`prim szamok: ${primes}; ${count};${found}`);
+    }
+    const endTime = new Date();
+    const duration = endTime.getTime() - startTime.getTime();
+    //result.duration = 
+    console.log(`Method runtime: ${duration} milliseconds`);
+    //return result
+  }
 }
